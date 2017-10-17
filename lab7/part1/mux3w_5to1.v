a992c094cfde8a3e818c677cb7c26840e001fb13
Kevin Tran <ktran@Kevins-MacBook-Pro.local>
2017-10-17 14:45:41 -0700

    fuck

--- lab7/part1/mux3w_5to1.v
+++ lab7/part1/mux3w_5to1.v
@@ -8,14 +8,10 @@ module Mux3W_5to1 (
        output    [2:0] LEDG       // Green LED outputs
        );
 
-//Code for the 3 bit 5-1 multiplexer
-
-       // Declaration of any needed reg or wire signals
-       wire [2:0] muxOut;   // using conditional assignments
+//Code for the 3 bit 5-1 multiplexe
 
        // Assign outputs 
-       assign LEDG = muxOut;
-       assign muxOut = SW[17:15] == 3'b000 ? SW[14:12] :
+       assign LEDG =   SW[17:15] == 3'b000 ? SW[14:12] :
                        SW[17:15] == 3'b001 ? SW[11: 9] :
                        SW[17:15] == 3'b010 ? SW[8:6]   :
                        SW[17:15] == 3'b011 ? SW[5:3]   :   
